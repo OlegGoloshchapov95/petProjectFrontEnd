944708765dff1cd00df850e6aa8cd1001ecf0936
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-03-21 19:03:23 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useRegisterUserMutation} from './redux';
+import {useEffect} from "react";
+import {useForm, Controller} from "react-hook-form"
+
+type FormData = {
+    userName: string
+    email: string
+    password: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [registerUserTrigger, registerUserResult] = useRegisterUserMutation()
+
+    const {handleSubmit, control} = useForm<FormData>({
+        mode: "onChange",
+    })
+
+    const onFormSubmit = (data: FormData) => {
+        registerUserTrigger({
+            "username": data.userName? data.userName : "",
+            "email": data.email? data.email : "",
+            "password": data.password? data.password : ""
+        })
+    }
+
+    useEffect(() => {
+        console.log(JSON.stringify(registerUserResult))
+    }, [registerUserResult])
+
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <form
+                className={"form-block"}
+                onSubmit={handleSubmit(onFormSubmit)}
+            >
+                <div className={"form-item"}>
+                    <label>User name</label>
+                    <Controller
+                        render={({field, fieldState}) => {
+                            return (
+                                <input
+                                    {...(field && {...field})}
+                                    type="text"
+                                />
+                            )
+                        }}
+                        name="userName"
+                        control={control}
+                        defaultValue={""}
+                    />
+                </div>
+                <div className={"form-item"}>
+                    <label>Email</label>
+                    <Controller
+                        render={({field, fieldState}) => {
+                            return (
+                                <input
+                                    {...(field && {...field})}
+                                    type="text"
+                                />
+                            )
+                        }}
+                        name="email"
+                        control={control}
+                        defaultValue={""}
+                    />
+                </div>
+                <div className={"form-item"}>
+                    <label>Password</label>
+                    <Controller
+                        render={({field, fieldState}) => {
+                            return (
+                                <input
+                                    {...(field && {...field})}
+                                    type="text"
+                                />
+                            )
+                        }}
+                        name="password"
+                        control={control}
+                        defaultValue={""}
+                    />
+                </div>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/redux/todosApi.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const todosApi = createApi({
+    reducerPath: 'todosApi',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:1337'}),
+    endpoints: (build) => ({
+        registerUser: build.mutation({
+            query: (body) => ({
+                url: 'api/auth/local/register',
+                method: 'POST',
+                body,
+            })
+        }),
+        /*/api/auth/local/register*/
+        /*getGoods: build.query({
+            query: (limit = '') => `goods?${limit && `_limit=${limit}`}`,
+            providesTags: (result) => result
+                ? [
+                    ...result.map(({ id }) => ({ type: 'Products', id })),
+                    { type: 'Products', id: 'LIST' },
+                ]
+                : [{ type: 'Products', id: 'LIST' }],
+        }),
+        addProduct: build.mutation({
+            query: (body) => ({
+                url: 'goods',
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [{type: 'Products', id: 'LIST'}]
+        }),
+        deleteProduct: build.mutation({
+            query: (id) => ({
+                url: `goods/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: [{type: 'Products', id: 'LIST'}]
+        })*/
+    })
+});
+
+export const {useRegisterUserMutation/*useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation*/} = todosApi;

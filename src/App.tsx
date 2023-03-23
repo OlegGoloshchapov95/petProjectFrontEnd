d186b0e2aee514e27de7ee2ba447140748a0f8cd
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-03-23 18:18:52 +0200

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import {useRegisterUserMutation} from './redux'
-import {useEffect} from "react"
-import {useForm, Controller} from "react-hook-form"
-import InputText from "./components/Input/InputText"
 import {Route, Routes} from 'react-router-dom'
 import SignInForm from "./components/SignInForm/SignInForm"
 import SignUpForm from "./components/SignUpForm/SignUpForm"
 
-type FormData = {
-    userName: string
-    email: string
-    password: string
-}
-
 function App() {
-    const [registerUserTrigger, registerUserResult] = useRegisterUserMutation()
-
-    const {handleSubmit, control} = useForm<FormData>({
-        mode: "onChange",
-    })
-
-    const onFormSubmit = (data: FormData) => {
-        registerUserTrigger({
-            "username": data.userName ? data.userName : "",
-            "email": data.email ? data.email : "",
-            "password": data.password ? data.password : ""
-        })
-    }
-
-    useEffect(() => {
-        registerUserResult?.data?.jwt && localStorage.setItem("bearerTokenForTodos", `Bearer ${registerUserResult.data.jwt}`)
-    }, [registerUserResult])
-
     return (
         <div>
             <Routes>
@@ -41,67 +13,6 @@ function App() {
                 localStorage.removeItem("bearerTokenForTodos")
             }}>Logout
             </button>
-            <h2>Sign up</h2>
-            <form
-                className={"form-block"}
-                onSubmit={handleSubmit(onFormSubmit)}
-            >
-                <div className={"form-item"}>
-                    <label>User name</label>
-                    <Controller
-                        render={({field, fieldState}) => {
-                            return (
-                                <InputText
-                                    field={field}
-                                    type="text"
-                                    fullWidth="full"
-                                    placeholder={"user name"}
-                                />
-                            )
-                        }}
-                        name="userName"
-                        control={control}
-                        defaultValue={""}
-                    />
-                </div>
-                <div className={"form-item"}>
-                    <label>Email</label>
-                    <Controller
-                        render={({field, fieldState}) => {
-                            return (
-                                <InputText
-                                    field={field}
-                                    type="text"
-                                    fullWidth="full"
-                                    placeholder={"email"}
-                                />
-                            )
-                        }}
-                        name="email"
-                        control={control}
-                        defaultValue={""}
-                    />
-                </div>
-                <div className={"form-item"}>
-                    <label>Password</label>
-                    <Controller
-                        render={({field, fieldState}) => {
-                            return (
-                                <InputText
-                                    field={field}
-                                    type="password"
-                                    fullWidth="full"
-                                    placeholder={"password"}
-                                />
-                            )
-                        }}
-                        name="password"
-                        control={control}
-                        defaultValue={""}
-                    />
-                </div>
-                <button type="submit">Send</button>
-            </form>
         </div>
     );
 }
--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,17 +1,103 @@
 import styles from "./SignUpForm.module.scss"
 import {cc} from "../../utils/Classnames"
+import {useRegisterUserMutation} from '../../redux'
+import {Controller, useForm} from "react-hook-form"
+import InputText from "../Input/InputText"
+import {useEffect} from "react"
 
 interface SignUpFormProps {
+}
+
+type FormData = {
+	userName: string
+	email: string
+	password: string
 }
 
 function SignUpForm(props: SignUpFormProps) {
 	const {
 	} = props
 
+	const [registerUserTrigger, registerUserResult] = useRegisterUserMutation()
+
+	const {handleSubmit, control} = useForm<FormData>({
+		mode: "onChange",
+	})
+
+	const onFormSubmit = (data: FormData) => {
+		registerUserTrigger({
+			"username": data.userName ? data.userName : "",
+			"email": data.email ? data.email : "",
+			"password": data.password ? data.password : ""
+		})
+	}
+
+	useEffect(() => {
+		registerUserResult?.data?.jwt && localStorage.setItem("bearerTokenForTodos", `Bearer ${registerUserResult.data.jwt}`)
+	}, [registerUserResult])
+
 	return (
-		<div
+		<form
+			className={"form-block"}
+			onSubmit={handleSubmit(onFormSubmit)}
 		>
-		</div>
+			<h2>Sign up</h2>
+			<div className={"form-item"}>
+				<label>User name</label>
+				<Controller
+					render={({field, fieldState}) => {
+						return (
+							<InputText
+								field={field}
+								type="text"
+								fullWidth="full"
+								placeholder={"user name"}
+							/>
+						)
+					}}
+					name="userName"
+					control={control}
+					defaultValue={""}
+				/>
+			</div>
+			<div className={"form-item"}>
+				<label>Email</label>
+				<Controller
+					render={({field, fieldState}) => {
+						return (
+							<InputText
+								field={field}
+								type="text"
+								fullWidth="full"
+								placeholder={"email"}
+							/>
+						)
+					}}
+					name="email"
+					control={control}
+					defaultValue={""}
+				/>
+			</div>
+			<div className={"form-item"}>
+				<label>Password</label>
+				<Controller
+					render={({field, fieldState}) => {
+						return (
+							<InputText
+								field={field}
+								type="password"
+								fullWidth="full"
+								placeholder={"password"}
+							/>
+						)
+					}}
+					name="password"
+					control={control}
+					defaultValue={""}
+				/>
+			</div>
+			<button type="submit">Send</button>
+		</form>
 	)
 }
 

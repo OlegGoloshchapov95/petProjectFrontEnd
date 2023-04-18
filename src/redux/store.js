e78c8aa8019af98945e26d59774944cce8920025
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-04-18 18:29:51 +0300

    project in progress

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { forumApi } from './todosApi';
+import { configureStore } from '@reduxjs/toolkit'
+import { forumApi } from './forumApi'
 
 export const store = configureStore({
     reducer: {
         [forumApi.reducerPath]: forumApi.reducer,
     },
     middleware: (getDefaultMiddlware) => getDefaultMiddlware().concat(forumApi.middleware)
-});
+})
